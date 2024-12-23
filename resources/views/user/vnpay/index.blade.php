3bae9daab1aa5ff8e0db150630495cb3dc947b56
annhquan1207 <quan-phamanh@quan.phamanh>
2024-12-23 14:04:28 +0700

    off debug

--- resources/views/user/vnpay/index.blade.php
+++ resources/views/user/vnpay/index.blade.php
@@ -33,7 +33,7 @@
                 </div>
                 <div class="form-group">
                     <label for="amount">Số tiền</label>
-                    <input class="form-control" id="amount" name="amount" type="number"
+                    <input class="form-control" id="amount" name="amount"
                         value="{{ priceFormat($totalPayment) }}" readonly />
                 </div>
                 <div class="form-group">
